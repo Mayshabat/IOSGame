fd35d443c13f58cd17ace5058464df56d778009a
May shabat <mayshabat1@gmail.com>
2026-06-08 18:42:30 +0300

    Initial Commit

---
+++ IOS-GAME/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IOS-GAME
+//
+//  Created by Student14 on 08/06/2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ IOS-GAMETests/IOS_GAMETests.swift
@@ -0,0 +1,17 @@
+//
+//  IOS_GAMETests.swift
+//  IOS-GAMETests
+//
+//  Created by Student14 on 08/06/2026.
+//
+
+import Testing
+@testable import IOS_GAME
+
+struct IOS_GAMETests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
